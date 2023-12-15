2424e2f77b2fb749d74443d20a4d2327c42c150b
Marufchik <raufovmaruf555@gmail.com>
2023-12-15 22:48:49 +0500

    article type methodlari

--- src/main/java/com/example/controller/ArticleTypeController.java
+++ src/main/java/com/example/controller/ArticleTypeController.java
@@ -1,17 +1,18 @@
 package com.example.controller;
 
 import com.example.dto.ArticleTypeDTO;
+import com.example.enums.LangEnum;
 import com.example.enums.ProfileRole;
 import com.example.service.ArticleTypeService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,4 +24,28 @@ public class ArticleTypeController {
     public ResponseEntity<?> create(@Valid @RequestBody ArticleTypeDTO dto) {
         return ResponseEntity.ok(articleTypeService.create(dto));
     }
+
+    @PutMapping("/admin/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> updateAdmin(@PathVariable("id") Long id,
+                                          @RequestBody ArticleTypeDTO articleTypeDTO
+                                        ) {
+        return ResponseEntity.ok(articleTypeService.updateAdmin(id, articleTypeDTO));
+    }
+
+
+    @GetMapping("/list")
+    public ResponseEntity<?> getList(@RequestParam Integer page,
+                                     @RequestParam Integer size) {
+        Page<ArticleTypeDTO> response = articleTypeService.getList(page, size);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/byLang")
+    public ResponseEntity<?> getByLang(@RequestParam LangEnum lang) {
+        List<ArticleTypeDTO> response = articleTypeService.getByLang(lang);
+        return ResponseEntity.ok(response);
+
+    }
+
 }
